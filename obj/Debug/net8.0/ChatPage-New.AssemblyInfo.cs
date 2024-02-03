1decb6e1c98e2c9fbb47309d86090e28556c7e19
John Doe <johndoe@example.com>
2024-02-03 13:45:03 +0530

    A Simple ChatPage with Blazor Server and SignalR

--- obj/Debug/net8.0/ChatPage-New.AssemblyInfo.cs
+++ obj/Debug/net8.0/ChatPage-New.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ChatPage-New")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+90b43fadfcdab3edafffb7e9f6f01ef6837b0f11")]
 [assembly: System.Reflection.AssemblyProductAttribute("ChatPage-New")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ChatPage-New")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
